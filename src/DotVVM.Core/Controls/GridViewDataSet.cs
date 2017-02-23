004192b251a2d418627c6fa64750c28e90da58d4
Patrik Švikruha <patrik.svikruha@gmail.com>
2017-02-23 17:53:43 +0100

    update GridViewDataSet pageNumber

--- src/DotVVM.Core/Controls/GridViewDataSet.cs
+++ src/DotVVM.Core/Controls/GridViewDataSet.cs
@@ -7,150 +7,150 @@ using System.Reflection;
 
 namespace DotVVM.Framework.Controls
 {
-	public class GridViewDataSet<T> : IGridViewDataSet
-	{
-		public string SortExpression { get; set; }
+    public class GridViewDataSet<T> : IGridViewDataSet
+    {
+        public string SortExpression { get; set; }
 
-		public bool SortDescending { get; set; }
+        public bool SortDescending { get; set; }
 
-		public int PageNumber { get; set; }
+        public int PageNumber { get; set; } = 1;
 
-		public int PageSize { get; set; }
+        public int PageSize { get; set; }
 
-		public int TotalItemsCount { get; set; }
+        public int TotalItemsCount { get; set; }
 
-		public IList<T> Items { get; set; }
+        public IList<T> Items { get; set; }
 
-		public string PrimaryKeyPropertyName { get; set; }
+        public string PrimaryKeyPropertyName { get; set; }
 
-		public object EditRowId { get; set; }
+        public object EditRowId { get; set; }
 
-		public virtual IList<int> NearPageNumbers
-		{
-			get {
-				return Enumerable.Range(1, PagesCount)
-				  .Where(n => Math.Abs(n - PageNumber) <= 5).ToList();
-			}
-		}
+        public virtual IList<int> NearPageNumbers
+        {
+            get {
+                return Enumerable.Range(1, PagesCount)
+                  .Where(n => Math.Abs(n - PageNumber) <= 5).ToList();
+            }
+        }
 
-		public int PagesCount
-		{
-			get
-			{
-				if (TotalItemsCount == 0 || PageSize == 0)
-				{
-					return 0;
-				}
-				return (int)Math.Ceiling((double)TotalItemsCount / PageSize);
-			}
-		}
+        public int PagesCount
+        {
+            get
+            {
+                if (TotalItemsCount == 0 || PageSize == 0)
+                {
+                    return 0;
+                }
+                return (int)Math.Ceiling((double)TotalItemsCount / PageSize);
+            }
+        }
 
-		public bool IsFirstPage => PageNumber == 1;
+        public bool IsFirstPage => PageNumber == 1;
 
-		public bool IsLastPage => PageNumber == PagesCount;
+        public bool IsLastPage => PageNumber == PagesCount;
 
-		IList IDotVVMDataSet.Items => (IList)Items; 
-		
+        IList IDotVVMDataSet.Items => (IList)Items; 
+        
 
-		public GridViewDataSet()
-		{
-			Items = new List<T>();
-		}
+        public GridViewDataSet()
+        {
+            Items = new List<T>();
+        }
 
-		public void GoToFirstPage()
-		{
-			PageNumber = 1;
-		}
+        public void GoToFirstPage()
+        {
+            PageNumber = 1;
+        }
 
-		public void GoToPreviousPage()
-		{
-			if (!IsFirstPage)
-			{
-				PageNumber--;
-			}
-		}
+        public void GoToPreviousPage()
+        {
+            if (!IsFirstPage)
+            {
+                PageNumber--;
+            }
+        }
 
-		public void GoToLastPage()
-		{
-			PageNumber = PagesCount;
-		}
+        public void GoToLastPage()
+        {
+            PageNumber = PagesCount;
+        }
 
-		public void GoToNextPage()
-		{
-			if (!IsLastPage)
-			{
-				PageNumber++;
-			}
-		}
+        public void GoToNextPage()
+        {
+            if (!IsLastPage)
+            {
+                PageNumber++;
+            }
+        }
 
-		public void GoToPage(int number)
-		{
-			PageNumber = number;
-		}
+        public void GoToPage(int number)
+        {
+            PageNumber = number;
+        }
 
-		public void Reset()
-		{
-			PageNumber = 1;
-		}
+        public void Reset()
+        {
+            PageNumber = 1;
+        }
 
-		public virtual void SetSortExpression(string expression)
-		{
-			if (SortExpression == expression)
-			{
-				SortDescending = !SortDescending;
-				GoToFirstPage();
-			}
-			else
-			{
-				SortExpression = expression;
-				SortDescending = false;
-				GoToFirstPage();
-			}
-		}
+        public virtual void SetSortExpression(string expression)
+        {
+            if (SortExpression == expression)
+            {
+                SortDescending = !SortDescending;
+                GoToFirstPage();
+            }
+            else
+            {
+                SortExpression = expression;
+                SortDescending = false;
+                GoToFirstPage();
+            }
+        }
 
-		public virtual void LoadFromQueryable(IQueryable<T> queryable)
-		{
-			TotalItemsCount = queryable.Count();
+        public virtual void LoadFromQueryable(IQueryable<T> queryable)
+        {
+            TotalItemsCount = queryable.Count();
 
-			if (!string.IsNullOrEmpty(SortExpression))
-			{
-				queryable = ApplySortExpression(queryable);
-			}
+            if (!string.IsNullOrEmpty(SortExpression))
+            {
+                queryable = ApplySortExpression(queryable);
+            }
 
-			if (PageSize > 0)
-			{
-				queryable = queryable.Skip(PageSize * (PageNumber - 1)).Take(PageSize);
-			}
+            if (PageSize > 0)
+            {
+                queryable = queryable.Skip(PageSize * (PageNumber - 1)).Take(PageSize);
+            }
 
-			Items = queryable.ToList();
-		}
+            Items = queryable.ToList();
+        }
 
-		public virtual IQueryable<T> ApplySortExpression(IQueryable<T> queryable)
-		{
-			var type = typeof(T);
-			var property = type.GetTypeInfo().GetProperty(SortExpression);
+        public virtual IQueryable<T> ApplySortExpression(IQueryable<T> queryable)
+        {
+            var type = typeof(T);
+            var property = type.GetTypeInfo().GetProperty(SortExpression);
 
-			if (property == null)
-			{
-				throw new Exception($"Could not sort by property '{SortExpression}', since it does not exists.");
-			}
+            if (property == null)
+            {
+                throw new Exception($"Could not sort by property '{SortExpression}', since it does not exists.");
+            }
 
-			var parameter = Expression.Parameter(type, "p");
-			var propertyAccess = Expression.MakeMemberAccess(parameter, property);
-			var orderBy = Expression.Lambda(propertyAccess, parameter);
+            var parameter = Expression.Parameter(type, "p");
+            var propertyAccess = Expression.MakeMemberAccess(parameter, property);
+            var orderBy = Expression.Lambda(propertyAccess, parameter);
 
-			var result = Expression.Call(typeof(Queryable),
-				GetSortingMethodName(),
-				new[] { type, property.PropertyType },
-				queryable.Expression,
-				Expression.Quote(orderBy));
+            var result = Expression.Call(typeof(Queryable),
+                GetSortingMethodName(),
+                new[] { type, property.PropertyType },
+                queryable.Expression,
+                Expression.Quote(orderBy));
 
-			return queryable.Provider.CreateQuery<T>(result);
-		}
+            return queryable.Provider.CreateQuery<T>(result);
+        }
 
-		private string GetSortingMethodName()
-		{
-			return SortDescending ? "OrderByDescending" : "OrderBy";
-		}
-	}
+        private string GetSortingMethodName()
+        {
+            return SortDescending ? "OrderByDescending" : "OrderBy";
+        }
+    }
 }
